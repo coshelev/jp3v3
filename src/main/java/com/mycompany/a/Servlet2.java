40227d51f582286117eef199c9a4bc5aca220e2e
coshelev <coshelev@gmail.com>
2021-12-09 14:16:27 +0300

    initial adding files

---
+++ src/main/java/com/mycompany/a/Servlet2.java
@@ -0,0 +1,22 @@
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+public class Servlet2 extends HttpServlet
+{
+    private String greeting="From servlet2";
+    public Servlet2(){}
+    public Servlet2(String greeting)
+    {
+        this.greeting=greeting;
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        response.setContentType("text/html");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println("<h1>"+greeting+"</h1>");
+        response.getWriter().println("session=" + request.getSession(true).getId());
+    }
+}
