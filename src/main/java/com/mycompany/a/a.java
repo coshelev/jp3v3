bc1fd267a7015a6b68f54be9a27d05bc0d8c9952
coshelev <32869772+coshelev@users.noreply.github.com>
2022-02-03 12:03:59 +0300

    10

--- src/main/java/com/mycompany/a/a.java
+++ src/main/java/com/mycompany/a/a.java
@@ -5,19 +5,18 @@ class A{
 	public static EventSource eventSource;
 
 	public static void main(String[] args){
-		System.out.print("class A constructor \n");
 		System.out.print ("	class A before jetty\n");
 		JettyServer server = new JettyServer();
-
 		try {
         		server.start();
         	} catch (Exception e) {           
             		e.printStackTrace();
         	}
-
 		System.out.print ("class A constructor end\n");
 		return;  
 	}
 
-	
+	public void emitEvent(String dataToSend){
+		eventSource.emitEvent(dataToSend);   
+    }
 }
