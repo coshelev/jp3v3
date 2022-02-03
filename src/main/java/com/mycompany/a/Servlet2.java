1ab7566d1106ff4aba19e3d391511a7354de1ecd
coshelev <32869772+coshelev@users.noreply.github.com>
2022-02-03 10:41:42 +0300

    3

--- src/main/java/com/mycompany/a/Servlet2.java
+++ src/main/java/com/mycompany/a/Servlet2.java
@@ -17,7 +17,6 @@ public class Servlet2 extends HttpServlet
         response.setContentType("text/html");
         response.setStatus(HttpServletResponse.SC_OK);
         response.getWriter().println("<h1>"+greeting+"</h1>");
-        response.getWriter().println("<script>var eventSource = new EventSource(""/ssevent""); eventSource.onmessage = function(event){console.log(event.data)};</script>");
         response.getWriter().println("session=" + request.getSession(true).getId());
     }
 }
